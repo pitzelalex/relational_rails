5d0a6e85ee31f003b95d507591771fc4c835d355
Alex Pitzel <pitzelalex@gmail.com>
2022-11-28 21:23:34 -0500

    feat: Create individual team page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,4 +4,8 @@ class TeamsController < ApplicationController
   def index
     @teams = Team.all
   end
+
+  def show
+    @team = Team.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
   get '/teams', to: 'teams#index'
+  get '/teams/:id', to: 'teams#show'
 end
