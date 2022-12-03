5a800ef7a12b57b9a9636c321bb955d7cba732a5
Alex Pitzel <pitzelalex@gmail.com>
2022-12-02 20:18:38 -0500

    test: Create test for form existance

--- spec/features/teams/new_spec.rb
+++ spec/features/teams/new_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe 'teams new page' do
   describe 'as a visitor' do
     describe "I visit '/teams/new'" do
-      it 'has content' do
+      it 'has a form' do
         visit '/teams/new'
 
         expect(page).to have_content('Create A New Team')
+        expect(page).to have_field('teamname')
+        expect(page).to have_field('championship')
+        expect(page).to have_field('racewins')
       end
     end
   end
