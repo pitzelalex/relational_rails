935d14acace9acf5097278cae6635367a78cbd57
Alex Pitzel <pitzelalex@gmail.com>
2022-12-02 21:01:53 -0500

    test: Create new team 'creates a new team' test

--- spec/features/teams/new_spec.rb
+++ spec/features/teams/new_spec.rb
@@ -13,5 +13,21 @@ RSpec.describe 'teams new page' do
         expect(page).to have_selector('input[type=submit]')
       end
     end
+
+    it 'creates a new team' do
+      visit '/teams/new'
+      expect(Team.count).to eq(0)
+
+      fill_in 'teamname', :with => 'Test Team'
+      fill_in 'racewins', :with => '99'
+      find('input[type=submit]').click
+      expect(Team.count).to eq(1)
+      expect(Team.last.name).to eq('Test Team')
+      expect(Team.last.champion).to be false
+      expect(Team.last.race_wins).to eq(99)
+
+      expect(page).to have_current_path('/teams')
+      expect(page).to have_content('Test Team')
+    end
   end
 end
