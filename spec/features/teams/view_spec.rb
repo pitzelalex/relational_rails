027e7e1a367fb26773660aaff102515100669616
Alex Pitzel <pitzelalex@gmail.com>
2022-12-04 15:16:24 -0500

    test: Create teams view 'has an edit button'

--- spec/features/teams/view_spec.rb
+++ spec/features/teams/view_spec.rb
@@ -59,6 +59,19 @@ RSpec.describe 'teams view page', type: :feature do
         expect(page).to have_selector(:link_or_button, "#{team3.name} Drivers")
         expect(find_link("#{team3.name} Drivers")[:href]).to eq("/teams/#{team3.id}/drivers")
       end
+
+      it 'has an edit button that takes the user to the edit page' do
+        team1 = Team.create!(name: 'Red Bull', champion: true, race_wins: 92)
+
+        visit "/teams/#{team1.id}"
+
+        expect(page).to have_selector(:link_or_button, 'Update Team')
+        expect(find_link('Update Team')[:href]).to eq("/teams/#{team1.id}/edit")
+
+        click_on 'Update Team'
+
+        expect(page).to have_current_path("/teams/#{team1.id}/edit")
+      end
     end
   end
 end
