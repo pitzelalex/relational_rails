4bba641c6187535e95f4083eeba5d231c88501e0
Alex Pitzel <pitzelalex@gmail.com>
2022-12-05 19:10:53 -0500

    test: Create teams/drivers/new_spec creates new driver test

--- spec/features/teams/drivers/new_spec.rb
+++ spec/features/teams/drivers/new_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe 'teams/drivers new page' do
   describe 'as a visitor' do
     describe "I visit '/teams/:team_id/drivers/new'" do
       it 'has a form' do
-        team1 = Team.create!(name: 'Red Bull', champion: true, race_wins: 92)
+        team = Team.create!(name: 'Red Bull', champion: true, race_wins: 92)
 
-        visit "/teams/#{team1.id}/drivers/new"
+        visit "/teams/#{team.id}/drivers/new"
 
-        expect(page).to have_content("Create A New Driver For #{team1.name}")
+        expect(page).to have_content("Create A New Driver For #{team.name}")
         expect(page).to have_field('name')
         expect(page).to have_field('superlicense')
         expect(page).to have_field('previous_series')
@@ -17,20 +17,25 @@ RSpec.describe 'teams/drivers new page' do
       end
     end
 
-    xit 'creates a new team' do
-      visit '/teams/new'
-      expect(Team.count).to eq(0)
+    it 'creates a new team' do
+      team = Team.create!(name: 'Red Bull', champion: true, race_wins: 92)
 
-      fill_in 'teamname', :with => 'Test Team'
-      fill_in 'racewins', :with => '99'
+      visit "/teams/#{team.id}/drivers/new"
+      expect(team.drivers.count).to eq(0)
+
+      fill_in 'name', :with => 'Crash Test Dummy'
+      check 'superlicense'
+      select 'F1', :from => 'previous_series'
+      fill_in 'race_wins_in_series', :with => '69'
       find('input[type=submit]').click
-      expect(Team.count).to eq(1)
-      expect(Team.last.name).to eq('Test Team')
-      expect(Team.last.champion).to be false
-      expect(Team.last.race_wins).to eq(99)
 
-      expect(page).to have_current_path('/teams')
-      expect(page).to have_content('Test Team')
+      expect(page).to have_current_path("/teams/#{team.id}/drivers")
+
+      expect(team.drivers.count).to eq(1)
+      expect(Driver.last.name).to eq('Crash Test Dummy')
+      expect(Driver.last.superlicense).to be true
+      expect(Driver.last.previous_series).to eq('F1')
+      expect(Driver.last.race_wins_in_series).to eq('69')
     end
   end
 end
