f5e1d0045c185021d1b334fd7b32df000d8c1fa6
Alex Pitzel <pitzelalex@gmail.com>
2022-12-06 17:52:05 -0500

    test: Create drivers index delete button

--- spec/features/drivers/index_spec.rb
+++ spec/features/drivers/index_spec.rb
@@ -59,5 +59,34 @@ RSpec.describe 'drivers index page', type: :feature do
       expect(page).to have_link("Edit", href: "/drivers/#{driver1.id}/edit")
       expect(page).to have_link("Edit", href: "/drivers/#{driver2.id}/edit")
     end
+
+    it 'has a delete driver button for each driver' do
+      team = Team.create!(name: 'Red Bull', champion: true, race_wins: 92)
+      driver1 = team.drivers.create!(name: 'Perez', superlicense: true, previous_series: 'F1', race_wins_in_series: 4)
+      driver2 = team.drivers.create!(name: 'Leclerc', superlicense: true, previous_series: 'F1', race_wins_in_series: 5)
+
+      visit '/drivers'
+
+      expect(page).to have_link("Delete", href: "/drivers/#{driver1.id}")
+      expect(page).to have_link("Delete", href: "/drivers/#{driver2.id}")
+    end
+
+    it 'deletes the driver' do
+      team = Team.create!(name: 'Red Bull', champion: true, race_wins: 92)
+      driver1 = team.drivers.create!(name: 'Perez', superlicense: true, previous_series: 'F1', race_wins_in_series: 4)
+      driver2 = team.drivers.create!(name: 'Leclerc', superlicense: true, previous_series: 'F1', race_wins_in_series: 5)
+
+      visit '/drivers'
+      expect(page).to have_content(driver1.name)
+      expect(page).to have_content(driver2.name)
+      click_link('Delete', href: "/drivers/#{driver1.id}")
+      expect(page).to have_current_path('/drivers')
+      expect(page).not_to have_content(driver1.name)
+      expect(page).to have_content(driver2.name)
+      click_link('Delete', href: "/drivers/#{driver2.id}")
+      expect(page).to have_current_path('/drivers')
+      expect(page).not_to have_content(driver1.name)
+      expect(page).not_to have_content(driver2.name)
+    end
   end
 end
