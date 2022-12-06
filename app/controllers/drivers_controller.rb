ecd0bb5478780f6fec9f3268935b35423f1921ae
Alex Pitzel <pitzelalex@gmail.com>
2022-12-05 21:01:31 -0500

    feat: Add #update to drivers controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -12,4 +12,16 @@ class DriversController < ApplicationController
   def edit
     @driver = Driver.find(params[:id])
   end
+
+  def update
+    driver = Driver.find(params[:id])
+    driver.update(driver_params)
+    redirect_to "/drivers/#{driver.id}"
+  end
+
+  private
+
+  def driver_params
+    params.permit(:name, :superlicense, :previous_series, :race_wins_in_series)
+  end
 end
